5f7accdb1d4c49ee8be9d259cdec54175396b40f
mdabdulaziz6236 <md.abdulaziz6236@gmail.com>
2025-09-30 11:53:00 +0600

    Complited ControlledField and details is written in __get__form__data__.js file

--- src/Components/ControlledField/ControlledField.jsx
+++ src/Components/ControlledField/ControlledField.jsx
@@ -1,15 +1,29 @@
-import React, { useState } from 'react';
+import React, {  useState } from 'react';
 
 const ControlledField = () => {
 
-    const [password ,setPassword] = useState('Secret')
+    const [password ,setPassword] = useState('')
+    const [error, setError] = useState('')
     const handleSubmit = (e) => {
         e.preventDefault();
         
         console.log('submitted: ')
+
+        if(password.length < 6 ){
+            setError('Password must be 6 characters or longer.')
+        }else{
+            setError('')
+        }
     }
     const handlePasswordOnChange = e => {
         console.log(e.target.value)
+        setPassword(e.target.value)
+        // const newPassword = e.target.value
+        // if(newPassword.length < 6 ){
+        //     setError('password must be 6 characters or longer.')
+        // }else{
+        //     setError('')
+        // }
     }
     return (
         <div>
@@ -18,6 +32,7 @@ const ControlledField = () => {
                     <input type="password" onChange={handlePasswordOnChange} defaultValue={password} name="password" id="2" placeholder='Password' required /><br />
                     <input type="submit" value="Submit" />
                 </form>
+                <p style={{color: 'red'}}><small>{error}</small></p>
         </div>
     );
 };
--- src/__get__form__data__.js
+++ src/__get__form__data__.js
@@ -1,5 +1,11 @@
 /***
  * 1. e.target.[name of the input field].value
  * 2. use form action and formData in the action        handler. formData.get('name of the input field')
+ * 3. controlled component. one per each field.  useState on change the field . useful to dynamically handle error.
+ * 3.5.  handle all controlled field on one state object
+ * const [formData, setFormData] = ({
+ * name: '',
+ * password: '',
+ * phone: ''})
  * 
 */
